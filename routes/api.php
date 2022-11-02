8dbc05b07de39a1d787ca4652cd9889140efc9c7
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2022-11-02 19:44:37 +0100

    name external books endpoint

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('external-books', [BooksController::class, 'externalBooks']);
+Route::get('external-books', [BooksController::class, 'externalBooks'])->name('api.books.external');
