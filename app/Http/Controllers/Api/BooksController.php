fd482cb2ea5902ed1095ed9b7d8960ae0238266e
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2022-11-02 21:28:05 +0100

    verify response for no result

--- app/Http/Controllers/Api/BooksController.php
+++ app/Http/Controllers/Api/BooksController.php
@@ -12,6 +12,14 @@ class BooksController extends Controller
     public function externalBooks(GetBooksByNameRequest $request)
     {
         $data = json_decode((new BookService())->getExternalBooks($request?->name));
+        if (! $data) {
+            return response()->json([
+                'status_code' => 404,
+                'status' => 'not found',
+                'data' => $data
+            ])->setStatusCode(404);
+        }
+
         return response()->json([
             'data' => BookResource::collection($data)
         ]);
