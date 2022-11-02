f95aa252e403f4ab85e0af252e0cdd58506906ab
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2022-11-02 19:43:19 +0100

    add api resource

---
+++ app/Http/Resources/BookResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BookResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'name' => $this->name,
+            'isbn' => $this->isbn,
+            'authors' => $this->authors,
+            'number_of_pages' => $this->numberOfPages,
+            'publisher' => $this->publisher,
+            'country' => $this->country,
+            'release_date' => $this->released
+        ];
+    }
+}
