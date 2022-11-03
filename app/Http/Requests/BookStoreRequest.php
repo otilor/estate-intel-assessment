ceb73267034d05d8d36f475d875c35b296ab1723
Gabriel Akinyosoye <gabrielfemi799@gmail.com>
2022-11-03 08:45:37 +0100

    format failed error

--- app/Http/Requests/BookStoreRequest.php
+++ app/Http/Requests/BookStoreRequest.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class BookStoreRequest extends FormRequest
 {
@@ -33,5 +35,35 @@ class BookStoreRequest extends FormRequest
             'publisher' => 'required|string',
             'release_date' => 'required|date',
         ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages()
+    {
+        return [
+            'name.required' => 'Name is required',
+            'isbn.required' => 'ISBN is required',
+            'authors.required' => 'Authors are required',
+            'authors.*.required' => 'Authors are required',
+            'authors.*.distinct' => 'Authors must be unique',
+            'country.required' => 'Country is required',
+            'number_of_pages.required' => 'Number of pages is required',
+            'number_of_pages.integer' => 'Number of pages must be an integer',
+            'publisher.required' => 'Publisher is required',
+            'release_date.required' => 'Release date is required',
+            'release_date.date' => 'Release date must be a date',
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'message'   => 'Validation errors',
+            'data'      => $validator->errors()
+        ]));
     }
 }
